b0fc10a37cde8934c8ee0f8c5d2b8fd55d05baa7
Vrkoval <vrkoval@gmail.com>
2023-04-27 16:12:37 +0300

    notify

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
+import { Notify } from 'notiflix';
 const markup = {
   
   createOneCard(srcObject) {
