bcd55418da79cd90694342281c91c410360317e2
Viktoriya Koval <vrkoval@gmail.com>
2023-04-29 21:06:09 +0300

    new changes

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,26 +3,50 @@ import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 const OUR_URL = 'https://pixabay.com/api/';
 const KEY_API='35660252-a6a2123775fa2a75b6944ac36';
-export default class findApi {
+export default class getFotoApi {
         constructor(){
-            this.query = '';
-            this.page = 1;    
+            this.searchQuery = '';
+            this.page = 1; 
+            this.per_page=40;   
         }
         
-        getData() {
-            const URL = `${OUR_URL}?key=${KEY_API}&q=${this.query}&
-            image_type=photo&orientation=horizontal&
-            safesearch=true&per_page=40&page=${this.page}`;
-              
-           return fetch(URL)
-            .then(r => r.json())
-            .then(({ hits }) => {
-                this.incrementPage();
+        async fetchFoto() {
+            const findParams=new URLFindParams(
+                {
+                    key: this.#KEY_API,
+                    q: this.searchQuery,
+                    image_type: 'photo',
+                    orientation: 'horizontal',
+                    safesearch: true,
+                    per_page: this.per_page,
+                    page: this.page, 
+                }
+            )
+            const URL = `${OUR_URL}?${findParams}`;
+            try{
+                const response= await axios.get(URL)
+                this.incrementPage()
+                return response.data
+            } catch(error) {
+                Notify.failure('A request error ocurred!',
+                {
+                    timeout:2000,
+                    width:'400px',
+                    fontSize:'16px',
+                    position:'right-top',
+                },);
+            
+            }
+        }  
+        //    return fetch(URL)
+        //     .then(r => r.json())
+        //     .then(({ hits }) => {
+        //         this.incrementPage();
         
-                return hits;
+        //         return hits;
         
-            });
-        };
+        //     });
+        // };
             incrementPage() {
             this.page += 1;
         }
@@ -31,14 +55,14 @@ export default class findApi {
             }
         
         
-        get searchQuery() {
-            return this.query;
-        };
+        get query() {
+            return this.searchQuery;
+        }
         
-        set searchQuery(newQuery) {
-            this.query = newQuery;
-        };
-        };
+        set query(newQuery) {
+            this.searchQuery = newQuery;
+        }
+        }
   
   
   
